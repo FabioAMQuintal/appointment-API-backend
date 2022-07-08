4072f78eca025a9e4d5ef98066213f1a8567af3d
Fabio Quintal <faaquintal@gmail.com>
2022-07-08 01:02:08 -0300

    Possible fix to heroku's deploy error

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,9 @@
 import dotenv from "dotenv";
 
-const envFound = dotenv.config();
+/*const envFound = dotenv.config();
 if (envFound.error) {
   throw new Error("Couldn't find .env file");
-}
+}*/
 
 export default {
   api: {
